cf47912c8555735c407ae67157746bff8c4e9ccf
Zubair Alam <zubair.alam210@gmail.com>
2025-10-16 02:48:14 +0500

    Add files via upload

---
+++ CLASS NO 1/Class 1.sql
@@ -0,0 +1,23 @@
+﻿--1. Create a new database
+✅ Syntax
+CREATE DATABASE Class1;
+
+--2. Print your name
+✅ Syntax
+SELECT 'Zubair Alam' AS name;
+
+--3. Print your age
+✅ Syntax
+SELECT 28 AS age;
+
+--4. Add two numbers
+✅ Syntax
+SELECT 10 + 15 as Sum;
+
+--5. Get today's date
+✅ Syntax
+SELECT GETDATE() AS TodayDate;
+
+6--. Combine name and age in one formatted query
+✅ SyntaxS
+SELECT 'NAME: Zubair Alam, AGE: 29' AS info;
